37dec8918ce56ef20d56c46390492565e96b3c87
LucaRosafalco <lucarosafalco@gmail.com>
2020-11-19 14:35:09 +0100

    generate mdof shear building acceleration recordings using STEAD

---
+++ src/convert_waveforms.py
@@ -0,0 +1,69 @@
+import os
+
+import obspy
+import h5py
+from obspy import UTCDateTime
+import numpy as np
+from obspy.clients.fdsn.client import Client
+import matplotlib.pyplot as plt
+
+def make_stream(dataset):
+    '''
+    input: hdf5 dataset
+    output: obspy stream
+
+    '''
+    data = np.array(dataset)
+
+    tr_E = obspy.Trace(data=data[:, 0])
+    tr_E.stats.starttime = UTCDateTime(dataset.attrs['trace_start_time'])
+    tr_E.stats.delta = 0.01
+    tr_E.stats.channel = dataset.attrs['receiver_type']+'E'
+    tr_E.stats.station = dataset.attrs['receiver_code']
+    tr_E.stats.network = dataset.attrs['network_code']
+
+    tr_N = obspy.Trace(data=data[:, 1])
+    tr_N.stats.starttime = UTCDateTime(dataset.attrs['trace_start_time'])
+    tr_N.stats.delta = 0.01
+    tr_N.stats.channel = dataset.attrs['receiver_type']+'N'
+    tr_N.stats.station = dataset.attrs['receiver_code']
+    tr_N.stats.network = dataset.attrs['network_code']
+
+    tr_Z = obspy.Trace(data=data[:, 2])
+    tr_Z.stats.starttime = UTCDateTime(dataset.attrs['trace_start_time'])
+    tr_Z.stats.delta = 0.01
+    tr_Z.stats.channel = dataset.attrs['receiver_type']+'Z'
+    tr_Z.stats.station = dataset.attrs['receiver_code']
+    tr_Z.stats.network = dataset.attrs['network_code']
+
+    stream = obspy.Stream([tr_E, tr_N, tr_Z])
+
+    return stream
+ 
+def make_plot(tr, title='', ylab=''):
+    '''
+    input: trace
+    
+    '''
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(tr.times("matplotlib"), tr.data, "k-")
+    ax.xaxis_date()
+    fig.autofmt_xdate()
+    plt.ylabel('counts')
+    plt.title(title)
+    plt.show()
+    
+    
+if __name__ == '__main__': 
+
+    # reading one sample trace from STEAD
+    dtfl = h5py.File(file_name, 'r')
+    dataset = dtfl.get('data/109C.TA_20061103161223_EV') 
+
+    # convering hdf5 dataset into obspy sream
+    st = make_stream(dataset)
+    
+    # ploting the verical component of the raw data
+    make_plot(st[2], title='Raw Data', ylab='counts')
