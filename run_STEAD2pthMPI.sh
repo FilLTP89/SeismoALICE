f161f8db0e1e86b4d99654efe3c5d1d6ebd7c7a3
kltpk89 <filippo.gatti@centralesupelec.fr>
2021-03-12 13:30:19 +0100

    Merge branch 'devl' of github.com:FilLTP89/SeismoALICE into devl
    # Conflicts:
    #	run-gpu-bb.sh
    #	src/common_setup.py
    #	src/database_sae.py
    #	src/trainer_bbfl.py
    
    Signed-off-by: kltpk89 <filippo.gatti@centralesupelec.fr>

--- run_STEAD2pthMPI.sh
+++ run_STEAD2pthMPI.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-#SBATCH -J stead2pth
+#SBATCH -J STEAD2pthMPI
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --ntasks-per-node=8
-#SBATCH --gres=gpu:2
-#SBATCH --mem=80GB
-#SBATCH --time=24:00:00
-#SBATCH --partition=gpu
-#SBATCH --output=outputJob-stead2pth.txt
-#SBATCH --error=errorJob-stead2pth.txt 
+#SBATCH --ntasks=20
+#SBATCH --time=01:00:00
+#SBATCH --partition=cpu_short
+#SBATCH --output=outputJob-STEAD2pthMPI.txt
+#SBATCH --error=errorJob-STEAD2pthMPI.txt 
 #SBATCH --mail-type=FAIL
 #SBATCH --export=NONE 
-## 
-#source load_conda_env_gpu.sh ruche  
-## cd ${SLURM_SUBMIT_DIR}
+
+source $HOME/load_conda_env_gpu.sh
+cd ${SLURM_SUBMIT_DIR}
  
 export PYTHONPATH="./src"
-
-mpirun --use-hwthread-cpus -np 12 python3 ./src/STEADextractorMPI.py --dataroot=$HOME/Data/Filippo/aeolus/STEAD/waveforms_11_13_19.hdf5 --dataset='stead' --cutoff=1. --signalSize=4096 --latentSize=4 --nzd=128 --workers=2 --nsy=10000 --batchSize=50
+export STEADROOT="/gpfs/workdir/invsem03/STEAD/waveforms_11_13_19.hdf5 "
+mpirun --use-hwthread-cpus -np 20 python3 ./src/STEADextractorMPI.py --dataroot=${STEADROOT} --dataset='stead' --cutoff=1. --signalSize=4096 --latentSize=4 --nzd=128 --workers=2 --nsy=10000 --batchSize=50
