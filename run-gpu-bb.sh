ccd5f33561844d60161cf382aaf7ad40dd811815
kltpk89 <filippo.gatti@centralesupelec.fr>
2021-03-12 13:30:19 +0100

    Merge branch 'devl' of github.com:FilLTP89/SeismoALICE into devl
    # Conflicts:
    #	run-gpu-bb.sh
    #	src/common_setup.py
    #	src/database_sae.py
    #	src/trainer_bbfl.py
    
    Signed-off-by: kltpk89 <filippo.gatti@centralesupelec.fr>

--- run-gpu-bb.sh
+++ run-gpu-bb.sh
@@ -26,5 +26,5 @@ export PYTHONPATH="./src"
 
 #python3 ./src/aae_drive_bbfl.py --dataroot='./database/mdof' --dataset='nt4096_ls128_nzf8_nzd32.pth' --cutoff=1. --imageSize=4096 --latentSize=128  --niter=5002 --cuda --ngpu=1 --nzd=32 --rlr=0.0001 --glr=0.0001 --outf='./imgs' --workers=8 --nsy=100 --batchSize=10 --actions='./actions_bb.txt' --strategy='./strategy_bb.txt' 
 
-python ./src/aae_drive_bbfl.py --dataroot='./database/stead' --dataset='nt4096_ls4_nf8_nzd32.pth' --cutoff=1. --imageSize=4096 --latentSize=128  --niter=5000 --cuda --ngpu=1 --nzd=32 --rlr=0.0001 --glr=0.0001 --outf='./imgs' --workers=8 --nsy=5000 --batchSize=100 --actions='./actions_bb.txt' --strategy='./strategy_bb.txt'>log_bb.txt 
+python3 ./src/aae_drive_bbfl.py --dataroot='./database/stead' --dataset='nt4096_ls4_nf8_nzd32.pth' --cutoff=1. --imageSize=4096 --latentSize=128  --niter=5000 --cuda --ngpu=1 --nzd=32 --rlr=0.0001 --glr=0.0001 --outf='./imgs' --workers=8 --nsy=5000 --batchSize=100 --actions='./actions_bb.txt' --strategy='./strategy_bb.txt'>log_bb.txt 
 
