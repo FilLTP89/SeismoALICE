fa52ebd3a0a54bccb411c2eaff175cb79368974c
kltpk89 <filippo.gatti@centralesupelec.fr>
2021-03-18 11:00:23 +0100

    Rename  run file

---
+++ mpirun_STEAD2pthMPI_local.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH="./src"
+export STEADROOT="~/Data/Filippo/aeolus/STEAD/waveforms_11_13_19.hdf5 "
+mpirun --use-hwthread-cpus -np 20 python3 ./src/STEADextractorMPI.py --dataroot=${STEADROOT} --dataset='stead' --cutoff=1. --signalSize=4096 --latentSize=4 --nzd=128 --workers=2 --nsy=10000 --batchSize=50
