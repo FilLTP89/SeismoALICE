[BUGFIX] Bugfix for InstanceNorm mixte with spectral Normalization

src/common/common_nn.py
@@ -199,9 +199,9 @@ def cnn1d(in_channels,out_channels,\
     #    block.insert(0,AddNoise(dev=dev))
     
     if bn:
-        if normalization == torch.nn.InstanceNorm1d:
-            block = [normalization(copy.deepcopy(block[0]))]
-            block.append(InstanceNorm1d(out_channels, affine=True))
+        if normalization.func.__name__.find('InstanceNorm1d')!=-1:
+            block = [torch.nn.utils.spectral_norm(copy.deepcopy(block[0]))]
+            block.append(normalization(out_channels, affine=True))
         else:
             block.append(BatchNorm1d(out_channels))
 

src/net/dcgan/dcgan_dxdataparallele.py
@@ -226,7 +226,7 @@ class DCGAN_Dx_Lite(BasicDCGAN_DxDataParallel):
         lout = self.lout(nch = nc,
                     padding = pad, dilation = dil,\
                     kernel_size = ker, stride = std)
-        self.cnn += [nn.Conv1d(in_channels=channel[-1],out_channels=channel[-1],
+        self.cnn += [nn.Conv1d(in_channels=channel[-1],out_channels=1,
                         kernel_size = 3, stride = 1, padding=1, bias=False)]
         # self.cnn += [normalization(channel[-1])]
         