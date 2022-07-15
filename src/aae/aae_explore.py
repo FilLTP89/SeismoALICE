eae17a4706c53be8ce41828540aa68777b31557b
gjacquet <gottfried.jacquet@ens-paris-saclay.fr>
2022-07-15 10:44:28 +0200

    [CHANGE] Make change to the config file

--- src/aae/aae_explore.py
+++ src/aae/aae_explore.py
@@ -1,5 +1,5 @@
-import warnings
-warnings.filterwarnings("ignore")
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 import common.ex_common_setup as cs
 from test.simple_test.wgan.exp_wgan import StreamWGAN
 # from test.simple_test.alice import ALICE
@@ -7,7 +7,6 @@ def main():
     u'''[SETUP] common variables/datasets'''
     cv = cs.setup()
     locals().update(cv)
-    
 
     stream_wgan = StreamWGAN(cv)
     stream_wgan.stream_presentation()
