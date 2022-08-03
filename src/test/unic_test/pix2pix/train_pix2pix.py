2284e5b31b573770293acdb8f6af624ee30701e8
gjacquet <gottfried.jacquet@ens-paris-saclay.fr>
2022-08-03 11:04:26 +0200

    [ADD] Adding noise to the discriminator to force theme stabilize the training of the GAN loss

--- src/test/unic_test/pix2pix/train_pix2pix.py
+++ src/test/unic_test/pix2pix/train_pix2pix.py
@@ -55,10 +55,10 @@ class Pix2Pix(UnicTrainer):
             # 1. Pix2Pix
             wnx,*others = noise_generator(x.shape,y.shape,app.DEVICE,app.NOISE)
             x_inp = zcat(x,wnx)
-            y_gen      = self.gen_agent.Fxy(x_inp)
+            y_gen = self.gen_agent.Fxy(x_inp)
 
             Dreal_xy, Dfake_xy = self.disc_agent.discriminate_conjointe_xy(x,y,y_gen)
-            Dloss_xy    = self.bce_logit_loss(Dreal_xy.reshape(-1),o1l(Dfake_xy.reshape(-1))) +\
+            Dloss_xy= self.bce_logit_loss(Dreal_xy.reshape(-1),o1l(Dfake_xy.reshape(-1))) +\
                             self.bce_logit_loss(Dfake_xy.reshape(-1),o0l(Dfake_xy.reshape(-1)))
             
             # 2. Summation of losses
